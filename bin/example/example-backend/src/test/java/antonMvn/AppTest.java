e9e347819dae1ad7ddeb9813114daf48b0e0d4b4
antony <tehanovanton@gmail.com>
2021-05-30 23:08:09 +0300

    commit

---
+++ bin/example/example-backend/src/test/java/antonMvn/AppTest.java
@@ -0,0 +1,12 @@
+package antonMvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
